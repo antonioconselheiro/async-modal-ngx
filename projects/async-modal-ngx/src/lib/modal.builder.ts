07bb447ae9859c5bfc45297b42e21423c7b04fa2
António Conselheiro <canudos.sudestino@gmail.com>
2024-02-20 08:19:29 -0300

    expondo subject do builder apenas como observable

--- projects/async-modal-ngx/src/lib/modal.builder.ts
+++ projects/async-modal-ngx/src/lib/modal.builder.ts
@@ -8,7 +8,7 @@ import { ModalableDirective } from './modalable.directive';
 export class ModalBuilder<EntryType, ReturnType> {
 
   private static modalInjectSubject = new Subject<IModalMetadata<unknown, unknown>>();
-  static modalInject$ = ModalBuilder.modalInjectSubject;
+  static modalInject$ = ModalBuilder.modalInjectSubject.asObservable();
 
   private injectData: EntryType | null = null;
   private cssClasses: string[] = [];
